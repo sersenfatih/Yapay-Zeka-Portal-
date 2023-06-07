9e624b1c748c785008b60fab18e834ce510198dc
sersenfatih <sersenfatih@gmail.com>
2023-06-07 13:45:04 +0300

    Initial commit

---
+++ backend/src/api/homeslider/services/homeslider.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * homeslider service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::homeslider.homeslider');
